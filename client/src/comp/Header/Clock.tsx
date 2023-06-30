df4e110fd3e32ae75950b254c18c0824442f746f
samdo91 <[fkdlzkfka@naver.com]>
2023-06-30 16:41:54 +0900

    addProductPage 시계 만들고 헤더 nav손봄

---
+++ client/src/comp/Header/Clock.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+type ClockProps = {
+  setDate?: React.Dispatch<React.SetStateAction<Date>>;
+};
+
+function Clock({ setDate }: ClockProps) {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+      if (setDate) {
+        setDate(new Date());
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const year = time.getFullYear().toString().padStart(4, "0");
+  const month = (time.getMonth() + 1).toString().padStart(2, "0");
+  const day = time.getDate().toString().padStart(2, "0");
+  const hour = time.getHours().toString().padStart(2, "0");
+  const minute = time.getMinutes().toString().padStart(2, "0");
+
+  const formattedTime = `${year}/${month}/${day}/${hour}/${minute}`;
+
+  return (
+    <div>
+      <div>{formattedTime}</div>
+    </div>
+  );
+}
+
+export default Clock;
