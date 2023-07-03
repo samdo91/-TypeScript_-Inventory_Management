bd549529b371cd1867650675583fbabc7e24af1e
samdo91 <[fkdlzkfka@naver.com]>
2023-07-04 01:12:29 +0900

    신규입고 페이지 거의 다 만듬 서버에서 연결해 줘야함

--- client/src/comp/page/Inboundpage/AddInboundPage/AddInboundPage.tsx
+++ client/src/comp/page/Inboundpage/AddInboundPage/AddInboundPage.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
-import { InboundTY } from "../../../../types/inbound";
 import { Button, Form, Table } from "react-bootstrap";
 import Header from "../../../Header/HeaderPage";
 import {
-  loginStateAtom,
+  userDataAtom,
   loginModals,
   searchingModalAtom,
 } from "../../../../globalStateManagement/index";
@@ -14,40 +13,30 @@ import { productTY } from "../../../../types/product";
 import SearchingModal from "../../../SearchingModal/SearchingModal";
 
 function AddInboundpage() {
-  const [selectedOption, setSelectedOption] = useState<string>(""); // 라디오
-  const [currentCode, setCurrentCode] = useState<number>(0); // 제품번호
-  const [currentName, setCurrentName] = useState<string>(""); // 제품 이름
-  const [currentQuantity, setCurrentQuantity] = useState<number>(0); //제품 수량
-  const [currentUnitPrice, setCurrentUnitPrice] = useState<number>(0); // 제품 입고가
-  const [currentPurchasePrice, setCurrentPurchasePrice] = useState<number>(0); // 제품 출고가
+  const [currentNote, setCurrentNote] = useState<string>(""); // 비고
+  const [currentQuantity, setCurrentQuantity] = useState<number>(0); // 입고수량
 
   const [productData, setProductData] = useState<productTY[]>([]);
   const [loginModal, setLoginModal] = useAtom(loginModals); // 로그인 모달 불러오기
-  const [loginState, setLoginState] = useAtom(loginStateAtom); //로그인 상태
+  const [userData, setUseData] = useAtom(userDataAtom);
   const [searchingModal, setSearchingModal] = useAtom(searchingModalAtom);
-
-  const handleOptionChange = (event: {
-    target: { value: React.SetStateAction<string> };
-  }) => {
-    setSelectedOption(event.target.value);
-  };
+  const [date, setDate] = useState<Date>(new Date()); //시간
 
   const addItem = () => {
     const NewItemInformation = {
-      //          :selectedOption,
-      //   productCode: currentCode,
-      //   productName: currentName,
-      //   quantity: currentQuantity,
-      //   unitPrice: currentUnitPrice,
-      //   purchasePrice: currentPurchasePrice,
+      addProductQuantity: currentQuantity, // 오더 품목 수
+      date: date,
+      product_id: productData[0].productCode,
+      employee_id: userData._id, // 입고자
+      note: currentNote,
     };
-    console.log(NewItemInformation);
   };
+
   return (
     <AddItemInformationPageBody>
       <HeaderSection>
         <header>
-          <Header />
+          <Header setDate={setDate} />
         </header>
       </HeaderSection>
 
@@ -124,61 +113,30 @@ function AddInboundpage() {
       </ProcuctSection>
 
       <ItemSection>
-        <div>
-          <div>code</div>
-          <Input
+        <InputFieldWrapper>
+          <div>주문수량</div>
+          <InputField
             type="text"
-            placeholder=" 제품 번호 입력"
-            value={currentCode}
-            onChange={(e) => {
-              setCurrentCode(parseInt(e.target.value));
-            }}
-          />
-        </div>
-        <div>
-          <div>name</div>
-          <Input
-            type="text"
-            placeholder=" 제품 이름 입력"
-            value={currentName}
-            onChange={(e) => {
-              setCurrentName(e.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <div>quantity</div>
-          <Input
-            type="text"
-            placeholder=" 재고 수량 입력"
+            placeholder="주문 수량"
             value={currentQuantity}
             onChange={(e) => {
-              setCurrentQuantity(parseInt(e.target.value));
+              const value = e.target.value;
+              setCurrentQuantity(value === "" ? 0 : parseInt(value));
             }}
           />
-        </div>
-        <div>
-          <div>unitPrice</div>
-          <Input
+        </InputFieldWrapper>
+        <InputFieldWrapper>
+          <div>상품 비고</div>
+          <InputField
             type="text"
-            placeholder="입고가 입력"
-            value={currentUnitPrice}
+            placeholder="상품 비고"
+            value={currentNote}
             onChange={(e) => {
-              setCurrentUnitPrice(parseInt(e.target.value));
+              const value = e.target.value;
+              setCurrentNote(value);
             }}
           />
-        </div>
-        <div>
-          <div>purchasePrice</div>
-          <Input
-            type="text"
-            placeholder="출고가 입력"
-            value={currentPurchasePrice}
-            onChange={(e) => {
-              setCurrentPurchasePrice(parseInt(e.target.value));
-            }}
-          />
-        </div>
+        </InputFieldWrapper>
       </ItemSection>
       <InputSection>
         <Buttons>입출고 삭제</Buttons>
@@ -189,6 +147,7 @@ function AddInboundpage() {
 }
 
 export default AddInboundpage;
+
 const AddItemInformationPageBody = styled.div``;
 const HeaderSection = styled.section``;
 const InputSection = styled.section`
@@ -199,8 +158,6 @@ const Title = styled.div`
   font-size: 30px;
 `;
 const ItemSection = styled.section``;
-const Input = styled.input``;
-const ProcuctSection = styled.section``;
 const InputField = styled(Form.Control)`
   width: 300px;
   margin: 0 auto;
@@ -211,3 +168,5 @@ const InputFieldWrapper = styled.div`
   flex-direction: column;
   margin: 10px;
 `;
+const ProcuctSection = styled.section``;
+const Input = styled.input``;
--- client/src/types/inbound.tsx
+++ client/src/types/inbound.tsx
@@ -8,8 +8,19 @@ export type InboundTY = {
   totalAmount: number;
 };
 export type InboundsTY = {
+  addProductQuantity: number; // 오더 품목 수
+  date: Date;
   product_id: string;
-  BusinessPartner_id: string;
-  quantity: number;
-  totalAmount: number;
+  employee_id: string; // 입고자
+  note: string;
 };
+
+// export type InboundsTY = {
+//   quantity: number;
+//   note: string;
+//   totalAmount: number;
+//   date: Date;
+//   product_id: string;
+//   BusinessPartner_id: string;
+//   retailPrice: number;
+// };
