f57f97fd095ed5e357feb4177ef6e3336f649a1c
samdo91 <[fkdlzkfka@naver.com]>
2023-07-03 20:37:08 +0900

    서치모달 중간 점검

--- client/src/comp/SearchingModal/SearchingModal.tsx
+++ client/src/comp/SearchingModal/SearchingModal.tsx
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
-import {
-  loginStateAtom,
-  loginModals,
-  searchingModalAtom,
-} from "../../globalStateManagement/index";
+import { searchingModalAtom } from "../../globalStateManagement/index";
 import { useAtom } from "jotai";
+import BulletinBoardComponent from "../BulletinBoardComponent/BulletinBoardComponent";
+import axios from "axios";
+import SearchingBar from "../SearchingBar/SearchingBar";
 
-function SearchingModal() {
+function SearchingModal(props: any) {
+  const PROXY =
+    window.location.hostname === "localhost"
+      ? "http://127.0.0.1:4000"
+      : "/proxy";
+  const { title, rowKey, dataListSearchingKey } = props;
   const [searchingModal, setSearchingModal] = useAtom(searchingModalAtom);
+  const [dataList, setDataList] = useState<any>("");
+
+  const handleDataList = async () => {
+    const response = await axios(`${PROXY}/${dataListSearchingKey}`);
+
+    setDataList([...response.data]);
+  };
+
+  useEffect(() => {
+    handleDataList();
+  }, []);
 
   const handleModalClose = () => {
     setSearchingModal(false);
@@ -16,14 +31,33 @@ function SearchingModal() {
 
   return (
     <>
-      <Modal show={searchingModal} onHide={handleModalClose}>
+      <Modal show={searchingModal} onHide={handleModalClose} size="lg">
         <Modal.Header closeButton>
-          <Modal.Title>My Modal</Modal.Title>
+          <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <p>This is the content of the modal.</p>
-          <p>You can add any JSX elements here.</p>
+          {dataList ? (
+            <BulletinBoardComponent
+              title={title}
+              dataList={dataList}
+              rowKey={rowKey}
+            />
+          ) : (
+            ""
+          )}
+
+          <SearchingBar
+            setDataList={setDataList}
+            keyList={[
+              "_id",
+              "productName",
+              "stock",
+              "wholesalePrice",
+              "retailPrice",
+            ]}
+            Theme="product"
+          />
         </Modal.Body>
 
         <Modal.Footer>
