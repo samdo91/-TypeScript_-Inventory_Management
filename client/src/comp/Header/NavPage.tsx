f3a93c8845e1e40e6ace80ecb210733f1ec058f2
samdo91 <[fkdlzkfka@naver.com]>
2023-07-06 02:07:06 +0900

    라우팅 다듬음

--- client/src/comp/Header/NavPage.tsx
+++ client/src/comp/Header/NavPage.tsx
@@ -12,12 +12,8 @@ function NavPage() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Link to="/ItemInformation">
-            <Nav.Link href="#contact">품목 정보</Nav.Link>
-          </Link>
-          <Link to="/BusinessPartner">
-            <Nav.Link href="#contact">거래처 정보</Nav.Link>
-          </Link>
+          <Nav.Link href="/ItemInformation">품목 정보</Nav.Link>
+          <Nav.Link href="/BusinessPartner">거래처 정보</Nav.Link>
           <Nav.Link href="/Inboundpage">입고</Nav.Link>
           <Nav.Link href="/OutboundPage">출고</Nav.Link>
           <Nav.Link href="#contact">손익확인</Nav.Link>
