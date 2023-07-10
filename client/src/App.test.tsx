bdc3f58cde5d6d7d940f2808f8b84b45342e8769
samdo91 <[fkdlzkfka@naver.com]>
2023-07-10 23:47:22 +0900

    리드미 수정 밑 배포를 위한 수정7

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,10 +1,11 @@
-const React = require("react");
-const { render } = require("@testing-library/react");
-const App = require("./App").default;
-const { getByText } = require("@testing-library/react");
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
 test("renders learn react link", () => {
-  render(React.createElement(App));
-  let linkElement = getByText(document.body, /learn react/i);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+export {};
