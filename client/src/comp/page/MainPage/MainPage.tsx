7447086732a15a0e0450665550676b53f6bf0d98
samdo91 <[fkdlzkfka@naver.com]>
2023-07-17 23:11:42 +0900

    테스트 파일  수정 밑 배포를 위한 수정6

--- client/src/comp/page/MainPage/MainPage.tsx
+++ client/src/comp/page/MainPage/MainPage.tsx
@@ -1,20 +1,19 @@
 import React, { useState, useEffect, useCallback } from "react";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import Header from "../../Header/HeaderPage";
-import styled from "@emotion/styled";
-import MainPageBoard from "./MainPageLInkButton/MainPageLInkButton";
+import MainPageLinkButton from "./MainPageLinkButton/MainPageLinkButton";
 import BulletinBoardComponent from "../../BulletinBoardComponent/BulletinBoardComponent";
-import { Container, Row, Col } from "react-bootstrap";
 import { productTY } from "../../../types/product";
 import { BusinessPartnerTY } from "../../../types/businessPartner";
 import axios from "axios";
 import { AddInboundTY } from "../../../types/inbound";
 
-function MainPage() {
+const MainPage = () => {
   const PROXY =
     window.location.hostname === "localhost"
       ? "http://127.0.0.1:4000"
       : "/proxy";
-  const [productList, setProductList] = useState<productTY[]>([]); //프로덕트 찾기 모달이랑 연결
+  const [productList, setProductList] = useState<productTY[]>([]);
   const [businessPartnerList, setBusinessPartnerList] = useState<
     BusinessPartnerTY[]
   >([]);
@@ -32,7 +31,6 @@ function MainPage() {
 
   const recentInbound = useCallback(async () => {
     const response = await axios(`${PROXY}/recentInbound`);
-    console.log(response);
     const updatedList = response.data.map((item: any) => {
       const { _id, ...rest } = item;
       return {
@@ -48,62 +46,75 @@ function MainPage() {
     recentBusinessPartner();
     recentInbound();
   }, [recentProducts, recentBusinessPartner, recentInbound]);
+
   return (
-    <div>
-      <header>
-        <Header />
-      </header>
-      <Container>
-        <Row>
-          <Col xs={12} md={4}>
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.mainContainer}>
+        <View style={styles.leftContainer}>
+          <ScrollView>
             {/* 좌측 영역 */}
-            <div style={{ height: "100vh" }}>
-              <MainPageBody>
-                <MainPageBoard />
-              </MainPageBody>
-            </div>
-          </Col>
-          <Col xs={12} md={8}>
+            <MainPageLinkButton />
+          </ScrollView>
+        </View>
+        <View style={styles.rightContainer}>
+          <ScrollView>
             {/* 우측 영역 */}
-            <div style={{ height: "100vh" }}>
-              <BulletinBoardComponent
-                title="최근 등록 품목"
-                dataList={productList}
-                rowKey={[
-                  "_id",
-                  "productName",
-                  "stock",
-                  "wholesalePrice",
-                  "retailPrice",
-                ]}
-                itemField="productName"
-              />
-              <BulletinBoardComponent
-                title="최근 등록 회사"
-                dataList={businessPartnerList}
-                rowKey={["_id", "BusinessPartnerName", "credit"]}
-                itemField={"BusinessPartnerName"}
-              />
-              <BulletinBoardComponent
-                title="최근 입고"
-                dataList={inboundList}
-                rowKey={[
-                  "inbound_id",
-                  "date",
-                  "product_id",
-                  "employee_id",
-                  "addProductQuantity",
-                ]}
-                itemField="inbound_id"
-              />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+            <BulletinBoardComponent
+              title="최근 등록 품목"
+              dataList={productList}
+              rowKey={[
+                "_id",
+                "productName",
+                "stock",
+                "wholesalePrice",
+                "retailPrice",
+              ]}
+              itemField="productName"
+            />
+            <BulletinBoardComponent
+              title="최근 등록 회사"
+              dataList={businessPartnerList}
+              rowKey={["_id", "BusinessPartnerName", "credit"]}
+              itemField={"BusinessPartnerName"}
+            />
+            <BulletinBoardComponent
+              title="최근 입고"
+              dataList={inboundList}
+              rowKey={[
+                "inbound_id",
+                "date",
+                "product_id",
+                "employee_id",
+                "addProductQuantity",
+              ]}
+              itemField="inbound_id"
+            />
+          </ScrollView>
+        </View>
+      </View>
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  mainContainer: {
+    flexDirection: "row",
+  },
+  leftContainer: {
+    flex: 1,
+    height: "100%",
+    padding: 10,
+  },
+  rightContainer: {
+    flex: 2,
+    height: "100%",
+    padding: 10,
+  },
+});
 
 export default MainPage;
-
-const MainPageBody = styled.body``;
